4882cb2a74272d5deaede00e7d59a3ab51c7e898
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2019-01-19 21:28:16 +0800

    Initial Commit

---
+++ airdropAll/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  airdropAll
+//
+//  Created by base on 19/1/19.
+//  Copyright © 2019 base. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ airdropAll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  airdropAll
+//
+//  Created by base on 19/1/19.
+//  Copyright © 2019 base. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
